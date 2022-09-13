0578911cee1e32e0f6eb3c37768a0d9e6352171d
filipe-barbosa <filipebarbosa94@hotmail.com>
2022-09-13 10:36:51 -0300

    chore: use react router link

--- src/components/pages/ContactDetails.tsx
+++ src/components/pages/ContactDetails.tsx
@@ -1,11 +1,11 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 export default function ContactDetails () {
   const { id } = useParams()
   return (
     <div>
       <h1>ContactDetails id: {id}</h1>
-      <a href="/">{'<- back'}</a>
+      <Link to="/">{'<- back'}</Link>
     </div>
   )
 }
